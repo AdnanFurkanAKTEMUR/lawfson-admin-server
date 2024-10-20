8363f209dd74edc0c6410e30feaa335ad7f55e8f
Adnan Furkan AKTEMUR <adnanfurkanaktemur@gmail.com>
2024-10-20 16:33:24 +0300

    type

--- dist/typeorm.config.js
+++ dist/typeorm.config.js
@@ -9,11 +9,13 @@ const Product_1 = require("./entities/Product");
 const Category_1 = require("./entities/Category");
 const AdminUser_1 = require("./entities/AdminUser");
 const Company_1 = require("./entities/Company");
+const AppUser_1 = require("./entities/AppUser");
+const Message_1 = require("./entities/Message");
 dotenv_1.default.config();
 exports.default = new typeorm_1.DataSource({
     type: "postgres",
     url: process.env.POSTGRES_CONNECTION_STRING,
-    entities: [Product_1.Product, Category_1.Category, AdminUser_1.AdminUser, Company_1.Company],
+    entities: [Product_1.Product, Category_1.Category, AdminUser_1.AdminUser, Company_1.Company, AppUser_1.AppUser, Message_1.Message],
     synchronize: true,
 });
 //# sourceMappingURL=typeorm.config.js.map
--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -4,12 +4,14 @@ import { Product } from "@entities/Product";
 import { Category } from "@entities/Category";
 import { AdminUser } from "@entities/AdminUser";
 import { Company } from "@entities/Company";
+import { AppUser } from "@entities/AppUser";
+import { Message } from "@entities/Message";
 
 dotenv.config();
 
 export default new DataSource({
   type: "postgres",
   url: process.env.POSTGRES_CONNECTION_STRING,
-  entities: [Product, Category, AdminUser, Company],
+  entities: [Product, Category, AdminUser, Company, AppUser, Message],
   synchronize: true,
 });
